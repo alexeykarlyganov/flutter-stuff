f5529aff80c5c125a310e86cc0d00f2b39aad907
Alexey Karlyganov <alexeykarlyganov@gmail.com>
2020-10-20 15:50:14 +0300

    added color messages

---
+++ shell/common.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+info() {
+	printf -- '\033[1;32m%s \033[0m\n' "$@"
+}
+
+warn() {
+	printf -- '\033[1;33m%s \033[0m\n' "$@"
+}
+
+error() {
+	printf -- '\033[1;31m%s \033[0m\n' "$@"
+}
+
+prompt() {
+	assume='ask'
+	printf -- '\033[1;33m%s \033[0m[y/N]: ' "$@"
+	if [ "$assume" = 'yes' ]; then
+		printf -- '%s\n' 'y'
+		return 0
+	elif [ "$assume" = 'no' ]; then
+		printf -- '%s\n' 'n'
+		return 1
+	else
+		read answer
+		case "$answer" in
+			[yY]|[yY][eE][sS]) return 0 ;;
+			*) return 1 ;;
+		esac
+	fi
+}
