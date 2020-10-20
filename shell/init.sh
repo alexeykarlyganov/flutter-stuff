6ceb2be83d23acda28d5826cf82a1bc757292562
Alexey Karlyganov <alexeykarlyganov@gmail.com>
2020-10-20 16:33:10 +0300

    move functions into file for remote run

--- shell/init.sh
+++ shell/init.sh
@@ -3,8 +3,28 @@
 # Exit on errors
 set -euo pipefail
 
-scriptDir=$(dirname "$(readlink -f "$0")")  
-source "$scriptDir"/common.sh
+# useful functions
+info() {
+	printf -- '\033[1;32m%s \033[0m\n' "$@"
+}
+
+prompt() {
+	assume='ask'
+	printf -- '\033[1;33m%s \033[0m[y/N]: ' "$@"
+	if [ "$assume" = 'yes' ]; then
+		printf -- '%s\n' 'y'
+		return 0
+	elif [ "$assume" = 'no' ]; then
+		printf -- '%s\n' 'n'
+		return 1
+	else
+		read answer
+		case "$answer" in
+			[yY]|[yY][eE][sS]) return 0 ;;
+			*) return 1 ;;
+		esac
+	fi
+}
 
 info "Installing required dependecies..."
 
